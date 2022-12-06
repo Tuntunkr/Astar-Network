b499b909a3f6e94ccbdd61aa2d8928c5145193a6
Tuntunkr <tuntunkumar04051998@gmail.com>
2022-12-06 21:16:26 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+    <section className="container-fluid">
+      <div className="conatiner">
+        <div className="Hero_heroContent">
+          <h1>The Future of <br class=""></br> Smart Contracts <br class="d-block d-sm-block d-md-block d-lg-none"></br>for Multichain</h1>
+          <p className="my-4 my-md-5">Astar is a multi-chain smart contract platform that supports multiple blockchains and virtual machines.</p>
+          <div>
+          <a href="/articles" >EXPLORE</a> 
+        </div>
+        </div>
+      </div>
+    </section>
+    </>
+  );
+}
